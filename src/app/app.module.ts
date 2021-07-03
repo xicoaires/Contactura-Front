ca98f76be713928f1377b38ce2841c13c2fbb3df
Francisco Aires <xicojr@gmail.com>
2021-07-03 17:24:25 -0300

    Aula 03/047/21

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { ListaContatosComponent } from './lista-contatos/lista-contatos.component';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
+import { FormUsuariosComponent } from './form-usuarios/form-usuarios.component';
+import { FormContatosComponent } from './form-contatos/form-contatos.component';
+import { NotFoundComponent } from './sharedComponents/not-found/not-found.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ListaContatosComponent,
+    ListaUsuariosComponent,
+    FormUsuariosComponent,
+    FormContatosComponent,
+    NotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
